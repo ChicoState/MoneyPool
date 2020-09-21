dfc6636766eb9079d816104c0f4b7ba898f3e70d
Kaleb Crisci <kcrisci94@gmail.com>
2020-09-21 14:53:50 -0700

    Initial Setup for Docker

---
+++ docker_readme.md
@@ -0,0 +1,7 @@
+#Setting up docker:
+
+* Step 1: Install Docker   
+* Step 2: Pull Initial Commit files to local computer
+* Step 3: Type the command 'docker build .' to set up environment from Dockerfile   
+* Step 4: Type the command 'docker-compose run web /bin/bash' to run the environment that was set up.   
+
---
+++ moneypool/moneypool/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for moneypool project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moneypool.settings')
+
+application = get_wsgi_application()

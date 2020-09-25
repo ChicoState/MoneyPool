0368b53f6825385850ace15f461f88e118a282ae
Kaleb Crisci <kcrisci94@gmail.com>
2020-09-24 18:05:35 -0700

    added email, username, and password sections

--- moneypool/myapp/models.py
+++ moneypool/myapp/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Profile(models.Model):
+class User(models.Model):
     firstName = models.CharField(max_length = 20)
     lastName = models.CharField(max_length = 20)
+    email = models.CharField(max_length = 50)
+    username = models.CharField(max_length = 20)
+    password = models.CharField(max_length = 20)
 
     def save(self, *args, **kwargs):
         super(myCustomUser, self).save(*args, **kwargs)

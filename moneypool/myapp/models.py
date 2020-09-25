0b248d95e38b605ad1a59508a3c9caa7a234a9b4
Kaleb Crisci <kcrisci94@gmail.com>
2020-09-24 18:46:10 -0700

    updated save function in user model

--- moneypool/myapp/models.py
+++ moneypool/myapp/models.py
@@ -8,9 +8,5 @@ class User(models.Model):
     username = models.CharField(max_length = 20)
     password = models.CharField(max_length = 20)
 
-    def save(self, *args, **kwargs):
-        super(myCustomUser, self).save(*args, **kwargs)
-        profile = myCustomUser(user = self)
-        profile.save()
         
 

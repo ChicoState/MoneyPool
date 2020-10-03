d68333a43da6e216b2351239fd014fa80565e7ac
Kaleb Crisci <kcrisci94@gmail.com>
2020-10-03 12:10:48 -0700

    Modified profile view to display trips a user creates

--- moneypool/myapp/views.py
+++ moneypool/myapp/views.py
@@ -4,20 +4,29 @@ from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 #from . import forms
-
+import datetime #for testing mytrips
 # Create your views here.
 
-
+class trips:  
+    def __init__(self, name, cost, date):  
+        self.name = name  
+        self.cost = cost 
+        self.date = date.strftime("%m/%d/%Y")
 #Login View
 @login_required(login_url='/login/')
 def index(request):
         if request.user.is_authenticated:
             u = User.objects.get(username=request.user.username)
+            mytrips = []
+            x = datetime.datetime(2020, 11, 15)
+            mytrips.append(trips("Costa Rica", 500, x))
+            x = datetime.datetime(2020, 12, 25)
+            mytrips.append(trips("Christmas Vacation", 800, x))
             context = {
                 "title":"My Profile",
                 "page_name":"Moneypool",
                 "name": u.first_name,
-                "data": u
+                "data": mytrips
             }
             return render(request, "profile.html", context=context)
         else:
